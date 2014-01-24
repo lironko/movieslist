f6b071eed90b060dedc077e2ea0eb5858aec5b5f
Liron <lironbackup@gmail.com>
2014-01-24 20:56:47 +0200

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Movieslist::Application.config.secret_key_base = 'd6a0fdf386d983eebbef1c848b85d208bf50559b5fc87f1d7c4736720e90514cc01eae7a85eae1e5f03f71b76a963a8e543b8a0ea7a599aae00bbe1561bbc40c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Movieslist::Application.config.session_store :cookie_store, key: '_movieslist_session'
